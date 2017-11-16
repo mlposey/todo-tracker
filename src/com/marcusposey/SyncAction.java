413abeeb245bedec828670e6d6546c8a2b3d0eb5
Marcus <marcusposey@gmail.com>
2017-11-16 14:40:36 -0600

    Initial commit

---
+++ src/com/marcusposey/SyncAction.java
@@ -0,0 +1,13 @@
+package com.marcusposey;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/** SyncAction initiates a sync of a project's todo items */
+public class SyncAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+
+    }
+}
