7abd368ea4de782575d7b916132b55e9a3d96cbc
Marcus <marcusposey@gmail.com>
2017-11-17 15:06:21 -0600

    Upload todos as GitHub issues

--- src/com/marcusposey/SyncAction.java
+++ src/com/marcusposey/SyncAction.java
@@ -5,23 +5,41 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 
+import java.io.IOException;
+
 /** SyncAction initiates a sync of a project's todo items */
 public class SyncAction extends AnAction {
 
+    /**
+     * Takes each todo comment from all .go project files and adds them
+     * to the GitHub repository as issues if they did not already
+     * exist.
+     */
     @Override
     public void actionPerformed(AnActionEvent event) {
         Project project = event.getProject();
         TodoService service = null;
+
         try {
             service = new TodoService(project);
         } catch (MissingRepoException exception) {
-            Messages.showMessageDialog(event.getProject(),
+            Messages.showMessageDialog(project,
                     "Could not find a repository named " + project.getName(),
                     "Error", Messages.getErrorIcon());
-            return;
+        } catch (IOException e) {
+            Messages.showMessageDialog(project,
+                    "Could not authenticate GitHub user", "Error",
+                    Messages.getErrorIcon());
         }
 
-        // Scanner scanner = new Scanner(project);
-        // service.push(scanner.scan());
+        Scanner scanner = new Scanner(project);
+
+        try {
+            service.push(scanner.scan());
+        } catch (IOException e) {
+            Messages.showMessageDialog(project,
+                    "Could not connect to repository",
+                    "Error", Messages.getErrorIcon());
+        }
     }
 }
--- src/com/marcusposey/TodoService.java
+++ src/com/marcusposey/TodoService.java
@@ -1,31 +1,99 @@
 package com.marcusposey;
 
+import com.google.gson.Gson;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.ui.Messages;
 import git4idea.GitUtil;
 import git4idea.repo.GitRepository;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.github.api.GithubApiUtil;
 import org.jetbrains.plugins.github.api.GithubConnection;
-import org.jetbrains.plugins.github.util.GithubAuthData;
+import org.jetbrains.plugins.github.api.GithubFullPath;
+import org.jetbrains.plugins.github.api.data.GithubIssue;
+import org.jetbrains.plugins.github.util.*;
+
+import java.io.IOException;
+import java.util.*;
 
 
 /** TodoServer pushes todo comments to GitHub as issues. */
 public class TodoService {
+    private final Project project;
+
+    // A URI to the issues POST endpoint
+    private String issuesPostUri;
+
+    private GithubFullPath userAndRepo;
     private final GithubConnection conn;
-    private final GitRepository repo;
+
+    private Gson gson = new Gson();
 
     /**
      * Creates a new service that is attached to a GitHub repository
      * @param project The local project
      * @throws MissingRepoException if a repository cannot be found that matches
      *                              the current project
+     * @throws IOException if GitHub authorization cannot be granted
      */
-    public TodoService(@NotNull Project project) throws MissingRepoException {
-        GithubAuthData authData = GithubAuthData.createFromSettings();
-        conn = new GithubConnection(authData, true);
+    public TodoService(@NotNull Project project) throws MissingRepoException,
+            IOException {
+        GithubAuthDataHolder authDataHolder = GithubUtil
+                .computeValueInModalIO(project, "Access to GitHub", indicator ->
+                    GithubUtil.getValidAuthDataHolderFromConfig(project,
+                            AuthLevel.LOGGED, indicator)
+        );
+        conn = new GithubConnection(authDataHolder.getAuthData(), true);
 
-        VirtualFile file = project.getProjectFile();
-        repo = GitUtil.getRepositoryManager(project).getRepositoryForFile(file);
+        GitRepository repo = GitUtil.getRepositoryManager(project)
+                .getRepositoryForFile(project.getProjectFile());
         if (repo == null) throw new MissingRepoException();
+
+        this.project = project;
+        extractMeta(repo);
+    }
+
+    /**
+     * Pushes todos to the project repository as issues
+     *
+     * If a todo has the same title as an existing issue's title, it will
+     * not be uploaded to the repository.
+     * @throws IOException if authentication fails
+     */
+    public void push(@NotNull List<Todo> todos) throws IOException {
+        List<GithubIssue> issues = GithubApiUtil.getIssuesAssigned(conn,
+                userAndRepo.getUser(), userAndRepo.getRepository(), "",
+                Integer.MAX_VALUE, false);
+
+        Set<String> issueTitles = new HashSet<>();
+        issues.forEach(iss -> issueTitles.add(iss.getTitle()));
+
+        // Submit only new issues.
+        todos.stream()
+                .filter(todo -> !issueTitles.contains(todo.title))
+                .forEach(this::submitNewIssue);
+    }
+
+    /** Submits the todo as a new issue to the project repository */
+    private void submitNewIssue(@NotNull Todo todo) {
+        try {
+            conn.postRequest(issuesPostUri, gson.toJson(todo));
+        } catch (IOException e) {
+            Messages.showMessageDialog(project, "Could not post issue:\n" +
+                gson.toJson(todo), "Error", Messages.getErrorIcon());
+        }
+    }
+
+    /** Extracts information about the repository */
+    private void extractMeta(GitRepository repo) throws IOException {
+        String url = GithubUtil.findGithubRemoteUrl(repo);
+        if (url == null) throw new IOException("Cannot find remote url");
+
+        userAndRepo = GithubUrlUtil.getUserAndRepositoryFromRemoteUrl(url);
+        if (userAndRepo == null) {
+            throw new IOException("Missing user or repository at remote URL");
+        }
+
+        issuesPostUri = "/repos/" + userAndRepo.getUser() + '/' +
+                userAndRepo.getRepository() + "/issues";
     }
 }
