225e06a67bd00174bc4a0bb1b45a5b45b3c9027d
Marcus <marcusposey@gmail.com>
2017-11-17 06:50:27 -0600

    Only match against the .go extension

--- src/com/marcusposey/Scanner.java
+++ src/com/marcusposey/Scanner.java
@@ -5,25 +5,28 @@ import com.intellij.openapi.roots.ProjectFileIndex;
 import com.intellij.openapi.vfs.VirtualFile;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /** Scanner searches project files for todo items */
 public class Scanner {
     private final Project project;
     private final Map<String, VirtualFile> files = new HashMap<>();
+    // The scanner only touches files with these extensions.
+    private final Set<String> exts = new HashSet<>();
 
     public Scanner(Project project) {
         this.project = project;
+        exts.add("go");
     }
 
     /** Scans all files in the project directory */
     public void scan() {
         ProjectFileIndex.SERVICE
                 .getInstance(project)
-                //.iterateContent(new ProjectFileIterator(this));
                 .iterateContent(file -> {
-                    if (!file.isDirectory()) {
-                        // Todo: Match against extensions.
+                    if (!file.isDirectory() && exts.contains(file.getExtension())) {
                         storeTodos(file);
                     }
                     return true;
